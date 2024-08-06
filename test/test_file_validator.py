428cd4ff13bfcc242dfc8a98f68c1e1008a84965
Zhijie Huang <zhihuan@microsoft.com>
2024-08-06 08:55:03 +0000

    test: fix test

--- test/test_file_validator.py
+++ test/test_file_validator.py
@@ -21,7 +21,7 @@ def test_file_validator_file_not_found_case_sensitive():
     validator = FileValidtor("TestCatalog", False, "license", [""], "test/data", [""], True, None)
     result, message = validator.validate()
     assert result == False
-    assert message == ItemResultFormat.FAIL.format(message="license File", detail_messages="- Error: license file is missing.")
+    assert message == ItemResultFormat.FAIL.format(message="license File", detail_messages=ItemResultFormat.SUBITEM.format(message="Error: license file is missing."))
 
 
 def test_file_validator_file_found_case_sensitive():
@@ -40,7 +40,7 @@ def test_file_validator_file_not_found_mutiple_extension():
     validator = FileValidtor("TestCatalog", False, "LICENSE", ["md", "txt"], "test/data", [""], False, None)
     result, message = validator.validate()
     assert result == False
-    assert message == ItemResultFormat.FAIL.format(message="LICENSE.md File", detail_messages="- Error: LICENSE.md file is missing.")
+    assert message == ItemResultFormat.FAIL.format(message="LICENSE.md File", detail_messages=ItemResultFormat.SUBITEM.format(message="Error: LICENSE.md file is missing."))
 
 def test_file_validator_file_found_in_subfolder():
     validator = FileValidtor("TestCatalog", False, "LICENSE", [""], "test", ["data"], False, None)
@@ -52,7 +52,7 @@ def test_file_validator_file_not_found_in_subfolder():
     validator = FileValidtor("TestCatalog", False, "license", [""], "test", ["data"], True, None)
     result, message = validator.validate()
     assert result == False
-    assert message == ItemResultFormat.FAIL.format(message="license File", detail_messages="- Error: license file is missing.")
+    assert message == ItemResultFormat.FAIL.format(message="license File", detail_messages=ItemResultFormat.SUBITEM.format(message="Error: license file is missing."))
 
 def test_file_validator_H2Tag_found():
     validator = FileValidtor("TestCatalog", False, "README", ["md"], "test/data", [""], True, ["## Test H2"])
@@ -64,4 +64,4 @@ def test_file_validator_H2Tag_not_found():
     validator = FileValidtor("TestCatalog", False, "README", ["md"], "test/data", [""], True, ["## Test H2 Not Found"])
     result, message = validator.validate()
     assert result == False
-    assert message == ItemResultFormat.FAIL.format(message="README.md File", detail_messages="- Error: ## Test H2 Not Found is missing in README.md.")
+    assert message == ItemResultFormat.FAIL.format(message="README.md File", detail_messages=ItemResultFormat.SUBITEM.format(message="Error: ## Test H2 Not Found is missing in README.md."))
