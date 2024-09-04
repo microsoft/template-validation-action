93b8cb4a14a256be5326fa480272c9c85e14c358
Zhijie Huang <zhihuan@microsoft.com>
2024-09-04 11:21:18 +0800

    feat: add retry logic for azd validator (#32)
    * fix python deps
    
    * feat: add retry logic for azd validator
    
    * fix: remove debugging message

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,25 @@
+import logging
 
 def indent(text, count=2):
     return (' ' * count).join(text.splitlines(True))
+
+def retry(times, retryMessages):
+    """
+    Decorator to retry a function multiple times if it raises a RetryableException
+    :param times: number of times to retry
+    :param retryMessages: list of strings that if found in the exception message, the function will be retried
+    """
+    def decorator(func):
+        def fn(*args, **kwargs):
+            attempt = 0
+            while attempt < times:
+                result, messages = func(*args, **kwargs)
+                if not result and any(message in messages for message in retryMessages):
+                    logging.warning(f"Retryable error message found in {func.__name__} output. Retrying.")
+                    attempt += 1
+                    logging.info(f"Retrying {func.__name__} {attempt}/{times}")
+                else:
+                    return result, messages
+            return func(*args, **kwargs)
+        return fn
+    return decorator
--- test/test_azd_validator.py
+++ test/test_azd_validator.py
@@ -12,6 +12,7 @@ class TestAzdValidator(unittest.TestCase):
         validator.validate()
         self.assertTrue(validator.result)
         self.assertIn(Signs.CHECK, validator.resultMessage)
+        self.assertEqual(mock_run.call_count, 1)
 
     @patch('subprocess.run')
     def test_azd_up_failure(self, mock_run):
@@ -20,6 +21,7 @@ class TestAzdValidator(unittest.TestCase):
         validator.validate()
         self.assertFalse(validator.result)
         self.assertIn(Signs.BLOCK, validator.resultMessage)
+        self.assertEqual(mock_run.call_count, 1)
 
     @patch('subprocess.run')
     def test_azd_down_success(self, mock_run):
@@ -28,6 +30,7 @@ class TestAzdValidator(unittest.TestCase):
         validator.validate()
         self.assertTrue(validator.result)
         self.assertIn(Signs.CHECK, validator.resultMessage)
+        self.assertEqual(mock_run.call_count, 1)
 
     @patch('subprocess.run')
     def test_azd_down_failure(self, mock_run):
@@ -36,3 +39,57 @@ class TestAzdValidator(unittest.TestCase):
         validator.validate()
         self.assertFalse(validator.result)
         self.assertIn(Signs.BLOCK, validator.resultMessage)
+        self.assertEqual(mock_run.call_count, 1)
+
+    @patch('subprocess.run')
+    def test_validate_retry_logic_ETXTBSY(self, mock_runCommand):
+        # Simulate a retryable error
+        mock_runCommand.side_effect = [
+            subprocess.CalledProcessError(1, "azd up", output="Retryable error message: spawn ETXTBSY"),
+            subprocess.CalledProcessError(1, "azd up", output="Retryable error message: spawn ETXTBSY"),
+            MagicMock(returncode=0, stdout="azd up success")
+        ]
+
+        validator = AzdValidator("AzdUpCatalog", ".", True, False)
+        validator.validate()
+
+        # Check that runCommand was called 3 times
+        self.assertTrue(validator.result)
+        self.assertIn(Signs.CHECK, validator.resultMessage)
+        self.assertEqual(mock_runCommand.call_count, 3)
+
+    @patch('subprocess.run')
+    def test_validate_retry_logic_DOCKER(self, mock_runCommand):
+        # Simulate a retryable error
+        mock_runCommand.side_effect = [
+            subprocess.CalledProcessError(1, "azd up", output="Retryable error message: Cannot connect to the Docker daemon"),
+            subprocess.CalledProcessError(1, "azd up", output="Retryable error message: Cannot connect to the Docker daemon"),
+            MagicMock(returncode=0, stdout="azd up success")
+        ]
+
+        validator = AzdValidator("AzdUpCatalog", ".", True, False)
+        validator.validate()
+
+        # Check that runCommand was called 3 times
+        self.assertTrue(validator.result)
+        self.assertIn(Signs.CHECK, validator.resultMessage)
+        self.assertEqual(mock_runCommand.call_count, 3)
+
+    @patch('subprocess.run')
+    def test_validate_retry_logic_finally_failed(self, mock_runCommand):
+        # Simulate a retryable error
+        mock_runCommand.side_effect = [
+            subprocess.CalledProcessError(1, "azd up", output="Retryable error message: Cannot connect to the Docker daemon"),
+            subprocess.CalledProcessError(1, "azd up", output="Retryable error message: Cannot connect to the Docker daemon"),
+            subprocess.CalledProcessError(1, "azd up", output="Retryable error message: Cannot connect to the Docker daemon"),
+            subprocess.CalledProcessError(1, "azd up", output="Retryable error message: Cannot connect to the Docker daemon"),
+        ]
+
+        validator = AzdValidator("AzdUpCatalog", ".", True, False)
+        validator.validate()
+
+        # Check that runCommand was called 3 times
+        self.assertFalse(validator.result)
+        self.assertEqual(mock_runCommand.call_count, 4)
+        self.assertIn(Signs.BLOCK, validator.resultMessage)
+        self.assertIn("Cannot connect to the Docker daemon", validator.resultMessage)
