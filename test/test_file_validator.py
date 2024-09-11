a143c19399461f176879e679170e8e411b7403fb
Zhijie Huang <zhihuan@microsoft.com>
2024-09-11 15:17:02 +0000

    test: fix ut

--- test/test_file_validator.py
+++ test/test_file_validator.py
@@ -6,38 +6,38 @@ from constants import ItemResultFormat, Signs
 
 
 def test_file_validator_file_found_exist():
-    validator = FileValidator("TestCatalog", "LICENSE", [""], "test/data", [""], None, False)
+    validator = FileValidator("TestCatalog", "LICENSE", [""], "test/data", ["."], None, False)
     result, message = validator.validate()
     assert result == True
     assert message == ItemResultFormat.PASS.format(message="LICENSE File")
 
 def test_file_validator_file_found_case_non_sensitive():
-    validator = FileValidator("TestCatalog", "license", [""], "test/data", [""], None, False)
+    validator = FileValidator("TestCatalog", "license", [""], "test/data", ["."], None, False)
     result, message = validator.validate()
     assert result == True
     assert message == ItemResultFormat.PASS.format(message="license File")
 
 def test_file_validator_file_not_found_case_sensitive():
-    validator = FileValidator("TestCatalog", "license", [""], "test/data", [""], None, True)
+    validator = FileValidator("TestCatalog", "license", [""], "test/data", ["."], None, True)
     result, message = validator.validate()
     assert result == False
     assert message == ItemResultFormat.FAIL.format(message="license File", detail_messages=ItemResultFormat.SUBITEM.format(sign=Signs.BLOCK, message="Error: license file is missing."))
 
 
 def test_file_validator_file_found_case_sensitive():
-    validator = FileValidator("TestCatalog", "LICENSE", [""], "test/data", [""], None, True)
+    validator = FileValidator("TestCatalog", "LICENSE", [""], "test/data", ["."], None, True)
     result, message = validator.validate()
     assert result == True
     assert message == ItemResultFormat.PASS.format(message="LICENSE File")
 
 def test_file_validator_file_found_mutiple_extension():
-    validator = FileValidator("TestCatalog", "LICENSE", ["md", "txt", ""], "test/data", [""], None, False)
+    validator = FileValidator("TestCatalog", "LICENSE", ["md", "txt", ""], "test/data", ["."], None, False)
     result, message = validator.validate()
     assert result == True
     assert message == ItemResultFormat.PASS.format(message="LICENSE.md File")
 
 def test_file_validator_file_not_found_mutiple_extension():
-    validator = FileValidator("TestCatalog", "LICENSE", ["md", "txt"], "test/data", [""], None, False)
+    validator = FileValidator("TestCatalog", "LICENSE", ["md", "txt"], "test/data", ["."], None, False)
     result, message = validator.validate()
     assert result == False
     assert message == ItemResultFormat.FAIL.format(message="LICENSE.md File", detail_messages=ItemResultFormat.SUBITEM.format(sign=Signs.BLOCK, message="Error: LICENSE.md file is missing."))
@@ -55,37 +55,37 @@ def test_file_validator_file_not_found_in_subfolder():
     assert message == ItemResultFormat.FAIL.format(message="license File", detail_messages=ItemResultFormat.SUBITEM.format(sign=Signs.BLOCK, message="Error: license file is missing."))
 
 def test_file_validator_H2Tag_found_case_non_sensitive():
-    validator = FileValidator("TestCatalog", "README", ["md"], "test/data", [""], ["## Test h2"], False)
+    validator = FileValidator("TestCatalog", "README", ["md"], "test/data", ["."], ["## Test h2"], False)
     result, message = validator.validate()
     assert result == True
     assert message == ItemResultFormat.PASS.format(message="README.md File")
 
 def test_file_validator_H2Tag_found_case_sensitive():
-    validator = FileValidator("TestCatalog", "README", ["md"], "test/data", [""], ["## Test H2"], True)
+    validator = FileValidator("TestCatalog", "README", ["md"], "test/data", ["."], ["## Test H2"], True)
     result, message = validator.validate()
     assert result == True
     assert message == ItemResultFormat.PASS.format(message="README.md File")
 
 def test_file_validator_H2Tag_not_found_case_non_sensitive():
-    validator = FileValidator("TestCatalog", "README", ["md"], "test/data", [""], ["## Test h2 Not Found"], False)
+    validator = FileValidator("TestCatalog", "README", ["md"], "test/data", ["."], ["## Test h2 Not Found"], False)
     result, message = validator.validate()
     assert result == False
     assert message == ItemResultFormat.FAIL.format(message="README.md File", detail_messages=ItemResultFormat.SUBITEM.format(sign=Signs.BLOCK, message="Error: ## Test h2 Not Found is missing in README.md."))
 
 def test_file_validator_H2Tag_not_found_case_sensitive():
-    validator = FileValidator("TestCatalog", "README", ["md"], "test/data", [""], ["## Test H2 Not Found"], True)
+    validator = FileValidator("TestCatalog", "README", ["md"], "test/data", ["."], ["## Test H2 Not Found"], True)
     result, message = validator.validate()
     assert result == False
     assert message == ItemResultFormat.FAIL.format(message="README.md File", detail_messages=ItemResultFormat.SUBITEM.format(sign=Signs.BLOCK, message="Error: ## Test H2 Not Found is missing in README.md."))
 
 def test_file_validator_folder_allowed_and_exists():
-    validator = FileValidator("TestCatalog", "ISSUE_TEMPLATE", ["md"], "test/data", [""], None, False, False, True)
+    validator = FileValidator("TestCatalog", "ISSUE_TEMPLATE", ["md"], "test/data", ["."], None, False, False, True)
     result, message = validator.validate()
     assert result == True
     assert message == ItemResultFormat.PASS.format(message="ISSUE_TEMPLATE Folder")
 
 def test_file_validator_folder_allowed_but_not_exists():
-    validator = FileValidator("TestCatalog", "NON_EXISTENT_FOLDER", ["md"], "test/data", [""], None, False, False, True)
+    validator = FileValidator("TestCatalog", "NON_EXISTENT_FOLDER", ["md"], "test/data", ["."], None, False, False, True)
     result, message = validator.validate()
     assert result == False
     assert message == ItemResultFormat.FAIL.format(message="NON_EXISTENT_FOLDER.md File or NON_EXISTENT_FOLDER Folder", detail_messages=ItemResultFormat.SUBITEM.format(sign=Signs.BLOCK, message="Error: NON_EXISTENT_FOLDER.md file or NON_EXISTENT_FOLDER folder is missing."))
--- test/test_result_aggregator.py
+++ test/test_result_aggregator.py
@@ -6,24 +6,28 @@ class TestResultAggregator(unittest.TestCase):
         aggregator = ResultAggregator()
 
         # Add mock results
-        aggregator.add_result("repository_management_readme", True, "README.md File")
-        aggregator.add_result("repository_management_license", True, "LICENSE.md File")
-        aggregator.add_result("repository_management_security", False, "SECURITY.md File")
-        aggregator.add_result("source_code_structure_azure_dev", True, "azure-dev.yaml File")
-        aggregator.add_result("functional_requirements_azd_up", True, "azd up")
-        aggregator.add_result("security_requirements_msdo", False, "MSDO validation failed")
+        aggregator.add_result("repository_management_readme", True, "- [x] README.md File")
+        aggregator.add_result("repository_management_license", True, "- [x] LICENSE.md File")
+        aggregator.add_result("repository_management_security", False, "- :warning: SECURITY.md File")
+        aggregator.add_result("source_code_structure_azure_dev", True, "- [x] azure-dev.yaml File")
+        aggregator.add_result("functional_requirements_azd_up", True, "- [x] azd up")
+        aggregator.add_result("security_requirements_msdo", False, "- :warning: MSDO validation failed")
 
         summary = aggregator.generate_summary()
 
         expected_summary = """# AI Gallery Standard Validation: FAILED
+
 ## Repository Management:
 - [x] README.md File
 - [x] LICENSE.md File
 - :warning: SECURITY.md File
+
 ## Source code structure and conventions:
 - [x] azure-dev.yaml File
+
 ## Functional Requirements:
 - [x] azd up
+
 ## Security Requirements:
 - :warning: MSDO validation failed"""
 
